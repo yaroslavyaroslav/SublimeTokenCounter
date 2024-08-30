f69ece22cd27953ecdda9cbd4eed51ec6672bcd8
Yaroslav Yashin <yaroslav.yashin@gmail.com>
2024-08-30 02:56:21 +0200

    Command togglable now

--- token_counter.py
+++ token_counter.py
@@ -1,10 +1,18 @@
 import sublime
 import sublime_plugin
-# import tiktoken
+import tiktoken
 
-class TokenCountCommand(sublime_plugin.TextCommand):
+VIEW_SETTINGS_KEY_TOKEN_COUNT_ENABLED = "token_count_enabled"
+
+class TokensCountCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        self.view.erase_phantoms("token_count")
+        view_settings = self.view.settings()
+        is_enabled = view_settings.get(VIEW_SETTINGS_KEY_TOKEN_COUNT_ENABLED, False)
+
+        if is_enabled:
+            self.close_phantom(None)  # Close existing phantom
+            view_settings.set(VIEW_SETTINGS_KEY_TOKEN_COUNT_ENABLED, False)
+            return
 
         # Get selected text
         selections = self.view.sel()
@@ -13,33 +21,22 @@ class TokenCountCommand(sublime_plugin.TextCommand):
             sublime.message_dialog("No text selected!")
             return
 
-        # Use a specific model's tokenizer; replace with the model you desire
-        # tokenizer = tiktoken.get_encoding("gpt-3.5-turbo")  # Update model as needed
 
-        total_token_count = 0
+        total_text = "\n".join([self.view.substr(sel) for sel in selections])
 
-        for sel in selections:
-            # selected_text = self.view.substr(sel)
-            # total_token_count += self.count_tokens(tokenizer, selected_text)
-            total_token_count = 3000
+        total_token_count = self.count_tokens(total_text)
 
         self.show_phantom(selections[0], total_token_count)
+        view_settings.set(VIEW_SETTINGS_KEY_TOKEN_COUNT_ENABLED, True)
 
-    # def count_tokens(self, tokenizer, text):
-    #     # Tokenize the text and return the token count
-    #     tokens = tokenizer.encode(text)
-    #     return len(tokens)
+    def count_tokens(self, text):
+        tokenizer = tiktoken.get_encoding("cl100k_base")
+        tokens = tokenizer.encode(text)
+        return len(tokens)
 
     def show_phantom(self, region, token_count):
         # Create the phantom content
-        phantom_content = f'<span style="color: lightgreen;">Total Tokens: </span> <span style="color: lightcoral;">{token_count}</span> <a href="close">[x]</a>'
-
-        # Create a phantom at the specified region
-        phantom = sublime.Phantom(
-            region,  # Position the phantom at the selection
-            phantom_content,
-            sublime.LAYOUT_INLINE  # Use inline layout for the phantom display
-        )
+        phantom_content = f'<span style="color: lightgreen;">Total Tokens:</span> <span style="color: lightcoral;">{token_count}</span> <a href="close">[x]</a>'
 
         # Use the add_phantom method to include the phantom
         self.view.add_phantom("token_count", region, phantom_content, sublime.LAYOUT_INLINE, on_navigate=self.close_phantom)
